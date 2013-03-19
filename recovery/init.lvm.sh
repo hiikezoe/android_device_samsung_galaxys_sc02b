ebee1f1b83b0812f6ebe1ca0eb69105e9de49d7a
Hiroyuki Ikezoe <hiikezoe@gnome.org>
2013-03-19 15:51:37 +0900

    moved.

---
+++ recovery/init.lvm.sh
@@ -0,0 +1,13 @@
+#!/sbin/sh
+LVM_VOLUMES=`/lvm/sbin/lvm lvs`
+if echo $LVM_VOLUMES | /sbin/grep -q -v 'system' &&
+   echo $LVM_VOLUMES | /sbin/grep -q -v 'userdata' ; then
+  LVM_PART='/dev/block/mmcblk0p2'
+  /lvm/sbin/lvm pvcreate $LVM_PART
+  /lvm/sbin/lvm vgcreate lvpool $LVM_PART
+  /lvm/sbin/lvm lvcreate -L 400M -n system lvpool
+  /lvm/sbin/lvm lvcreate -l 100%FREE -n userdata lvpool
+
+  /sbin/make_ext4fs -b 4096 -g 32768 -i 8192 -I 256 -l -16384 -a /system /dev/lvpool/system
+  /sbin/make_ext4fs -b 4096 -g 32768 -i 8192 -I 256 -l -16384 -a /data /dev/lvpool/userdata
+fi
